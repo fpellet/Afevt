833d4541a94ebb7629ee64409838a46355081897
fpellet <git@florentpellet.com>
2017-07-18 22:16:00 +0200

    Implement AfevtAnalyzer

---
+++ Afevt.Test/AfevtAnalyzerShould.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using Afevt;
+
+namespace Afevt.Test
+{
+    [TestClass]
+    public class AfevtAnalyzerShould : CodeFixVerifier
+    {
+        [TestMethod]
+        public void ReturnErrorIfCreateValueTypeWithDefaultConstructor()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        public struct ValueTypeA {
+            public int Value { get; }
+
+            public ValueTypeA(int value) {
+                Value = value;
+            }
+        }
+
+        class TypeName
+        {   
+            public void MethodA(){
+                var a = new ValueTypeA();
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "Afevt",
+                Message = "Default constructor is prohibited, because ValueTypeA has others constructors",
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                        new DiagnosticResultLocation("Test0.cs", 15, 25)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void ReturnNoErrorIfCreateValueTypeWithOtherConstructor()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        public struct ValueTypeA {
+            public int Value { get; }
+
+            public ValueTypeA(int value) {
+                Value = value;
+            }
+        }
+
+        class TypeName
+        {   
+            public void MethodA(){
+                var a = new ValueTypeA(4);
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void ReturnNoErrorIfCreateValueTypeWithDefautConstructorButNotOtherConstructorExists()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        public struct ValueTypeA {
+        }
+
+        class TypeName
+        {   
+            public void MethodA(){
+                var a = new ValueTypeA();
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void ReturnNoErrorIfCreateClassWithDefautConstructor()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        public class ValueTypeA {
+            public int Value { get; }
+
+            public ValueTypeA() {
+            }
+
+            public ValueTypeA(int value) {
+                Value = value;
+            }
+        }
+
+        class TypeName
+        {   
+            public void MethodA(){
+                var a = new ValueTypeA();
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void ReturnNoErrorIfCreateWithDefaultConstructorButValueTypeIsInSystemNamespace()
+        {
+            var test = @"
+    namespace System.Joe.Indien 
+    {
+        public struct ValueTypeA {
+            public int Value { get; set; }
+
+            public ValueTypeA(int value) {
+                Value = value;
+            }
+        }
+    }
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+            public void MethodA(){
+                var a = new System.Joe.Indien.ValueTypeA();
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void ReturnNoErrorIfCreateWithDefaultConstructorButValueTypeIsInMicrosoftNamespace()
+        {
+            var test = @"
+    namespace Microsoft.Joe.Indien 
+    {
+        public struct ValueTypeA {
+            public int Value { get; set; }
+
+            public ValueTypeA(int value) {
+                Value = value;
+            }
+        }
+    }
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+            public void MethodA(){
+                var a = new Microsoft.Joe.Indien.ValueTypeA();
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AfevtAnalyzer();
+        }
+    }
+}
---
+++ Afevt/AfevtAnalyzer.cs
@@ -0,0 +1,44 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Afevt
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AfevtAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            "Afevt",
+            "Avoid default constructor",
+            "Default constructor is prohibited, because {0} has others constructors",
+            "Struct",
+            DiagnosticSeverity.Error,
+            isEnabledByDefault: true,
+            description: "Default constructor is prohibited");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ObjectCreationExpression);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var node = (ObjectCreationExpressionSyntax)context.Node;
+            if (node.ArgumentList.Arguments.Any()) return;
+
+            var typeCreated = context.SemanticModel.GetSymbolInfo(node.Type).Symbol as INamedTypeSymbol;
+            if (typeCreated?.TypeKind != TypeKind.Struct) return;
+
+            var namespaceName = typeCreated.ContainingNamespace.ToString();
+            if (namespaceName.StartsWith("System") || namespaceName.StartsWith("Microsoft")) return;
+
+            if (typeCreated.Constructors.Length == 1) return;
+
+            context.ReportDiagnostic(Diagnostic.Create(Rule, node.GetLocation(), typeCreated.Name));
+        }
+    }
+}
